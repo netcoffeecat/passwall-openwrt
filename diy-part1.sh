d3b14829de9fe07608199b6a437a7009e2524ab9
netcoffeecat <62831356+netcoffeecat@users.noreply.github.com>
2021-02-17 00:54:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,4 +11,4 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 # echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/small
+# git clone https://github.com/kenzok8/small.git package/small
